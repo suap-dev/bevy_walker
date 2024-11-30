de3caf44e1ec20368541e097f0db5c52b5dc2560
suap-dev <suap.dev@gmail.com>
2024-11-30 21:27:14 +0100

    upgrade light to bevy-0.15 api

--- src/systems/light.rs
+++ src/systems/light.rs
@@ -4,19 +4,13 @@ pub mod startup {
     use crate::setup::LIGHT_POSITION;
 
     pub fn spawn(mut commands: Commands) {
-        commands.spawn(PointLightBundle {
-            point_light: PointLight {
+        commands.spawn((
+            PointLight {
                 shadows_enabled: true,
                 range: 60.0,
-                // color: todo!(),
-                // intensity: todo!(),
-                // radius: todo!(),
-                // shadow_depth_bias: todo!(),
-                // shadow_normal_bias: todo!(),
                 ..default()
             },
-            transform: Transform::from_translation(LIGHT_POSITION),
-            ..default()
-        });
+            Transform::from_translation(LIGHT_POSITION),
+        ));
     }
 }
